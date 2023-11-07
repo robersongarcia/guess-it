173aa87acef2a7671c0d9fd8cc4026837014d4b3
Roberson Garcia <roberson.gar12@gmail.com>
2023-11-07 14:22:50 -0400

    a

--- Client/src/pages/GamePage.tsx
+++ Client/src/pages/GamePage.tsx
@@ -1,8 +1,13 @@
 
 export const GamePage = () => {
   return (
-    <div className="w-screen h-screen">
-        
+    <div className="w-screen h-screen flex flex-row">
+        <div className="bg-red-500 w-4/6">
+
+        </div>
+        <div className="bg-blue-500 w-2/6">
+            
+        </div>
     </div>
   )
 }
